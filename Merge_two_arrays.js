26429e30137d8c38d42c4de0ca7774123cd781b5
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-12 18:54:17 +0500

    Update Merge_two_arrays.js
    https://stackoverflow.com/questions/13107855/how-to-check-if-an-array-index-exists-or-not-in-javascript/55503670#55503670

--- Merge_two_arrays.js
+++ Merge_two_arrays.js
@@ -3,8 +3,8 @@ function mergeArrays(a, b) {
   const longArrayLength = a.length > b.length ? a.length : b.length;
   const mix = [];
   for (let i = 0; i < longArrayLength; i++) {
-    if (a[i]) mix.push(a[i]); //won't work for falsey values
-    if (b[i]) mix.push(b[i]);
+    if (a.hasOwnProperty(i)) mix.push(a[i]); //won't work for falsey values
+    if (b.hasOwnProperty(i)) mix.push(b[i]);
   }
   return mix;
 }
