be8a14a6b2e01ec8eac31aae84da3368ca26cecd
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-14 21:52:37 +0500

    Update HighestAndLowestjs.js

--- HighestAndLowestjs.js
+++ HighestAndLowestjs.js
@@ -1,5 +1,5 @@
 function highAndLow(numbers) {
   // ...
-  const numbersArray = numbers.split(" ").map((item) => Number(item));
+  const numbersArray = numbers.split(" ").map(Number);
   return [Math.max(...numbersArray), Math.min(...numbersArray)].join(" ");
 }
