eab06ce491b38cf5b9ca509c4332a44192172513
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-14 22:04:59 +0500

    Update HighestAndLowestjs.js

--- HighestAndLowestjs.js
+++ HighestAndLowestjs.js
@@ -1,5 +1,14 @@
 function highAndLow(numbers) {
-  // ...
-  const numbersArray = numbers.split(" ").map(Number);
-  return [Math.max(...numbersArray), Math.min(...numbersArray)].join(" ");
+  const array = numbers.split(" ").map((item) => Number(item));
+  let [min, max] = array.reduce(
+    ([prevMin, prevMax], curr) => [
+      Math.min(prevMin, curr),
+      Math.max(prevMax, curr),
+    ],
+    [Infinity, -Infinity]
+  );
+  return `${max} ${min}`;
 }
+
+//https://stackoverflow.com/a/63514134/9513184
+// truth be told I haven't got it
