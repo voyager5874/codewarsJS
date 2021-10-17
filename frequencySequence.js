b5eeff94551cef0a809480ab74e0da055a418480
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-17 09:24:42 +0500

    Update frequencySequence.js

--- frequencySequence.js
+++ frequencySequence.js
@@ -3,24 +3,8 @@
 // freq_seq("^^^**$", "x"); // => "3x3x3x2x2x1"
 
 function freqSeq(str, sep) {
-  const count = {};
-  //const array = str.split('').filter(item=> item !== ' ');
-  const array = str.split("");
-  let res = "";
-  array.forEach((item) => {
-    if (count[item]) {
-      count[item] += 1;
-      return;
-    }
-    count[item] = 1;
-  });
-  array.forEach((item, index) => {
-    if (index <= array.length - 2) {
-      res += count[item];
-      res += sep;
-    } else {
-      res += count[item];
-    }
-  });
-  return res;
+  return str
+    .split("")
+    .map((char, index, arr) => arr.filter((item) => char === item).length)
+    .join(sep);
 }
