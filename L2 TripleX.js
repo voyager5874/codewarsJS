06bff68a9a1dbe58602611d8cf388422c884ffc6
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-20 22:47:35 +0500

    Update L2 TripleX.js
    account for the case 'x' is not present

--- L2 TripleX.js
+++ L2 TripleX.js
@@ -1,6 +1,4 @@
 function tripleX(str) {
   let firstIndex = str.indexOf("x");
-  return (
-    `${str[firstIndex]}${str[firstIndex + 1]}${str[firstIndex + 2]}` === "xxx"
-  );
+  return firstIndex !== -1 && firstIndex === str.indexOf("xxx");
 }
