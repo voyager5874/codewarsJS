c14ac9bdbf61bf083a0c94f9ca27717973304b15
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-20 20:32:40 +0500

    Update swap items in a dictionary.js
    spread operator

--- swap items in a dictionary.js
+++ swap items in a dictionary.js
@@ -1,15 +1,10 @@
 function switchDict(dic) {
-  const swapped = {};
-  Object.entries(dic)
-    .map((item) => item.reverse())
-    .forEach((pair) => {
-      if (!swapped.hasOwnProperty(pair[0])) {
-        swapped[pair[0]] = [pair[1]];
-        return;
-      } else {
-        swapped[pair[0]].push(pair[1]);
-      }
-    });
+  let keys = [...new Set(Object.keys(dic))];
+  const dic2 = {};
 
-  return swapped;
+  keys.forEach((i) =>
+    dic2[dic[i]] ? dic2[dic[i]].push(i) : (dic2[dic[i]] = [i])
+  );
+
+  return dic2;
 }
