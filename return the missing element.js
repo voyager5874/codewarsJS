6808884a0d66e88deeccc964ed4d4dff01b45679
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-21 18:45:30 +0500

    Update return the missing element.js

--- return the missing element.js
+++ return the missing element.js
@@ -1,14 +1,5 @@
 function getMissingElement(superImportantArray) {
-  const arr = superImportantArray.sort();
-  if (arr[0] !== 0) {
-    return 0;
-  }
-  if (arr[arr.length - 1] !== 9) {
-    return 9;
-  }
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] - 1 !== arr[i - 1]) {
-      return i;
-    }
+  for (i = 0; i < 10; i++) {
+    if (superImportantArray.indexOf(i) === -1) return i;
   }
 }
