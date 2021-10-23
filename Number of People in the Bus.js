f24dd3fd556d6b958625ea1df46828f8c8def840
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-23 08:49:27 +0500

    Update Number of People in the Bus.js
    destructuring the current

--- Number of People in the Bus.js
+++ Number of People in the Bus.js
@@ -1,8 +1,2 @@
-let number = function (busStops) {
-  let res = 0;
-  const arr = busStops.forEach((busStop) => {
-    res += busStop[0];
-    res -= busStop[1];
-  });
-  return res;
-};
+const number = (busStops) =>
+  busStops.reduce((rem, [on, off]) => rem + on - off, 0);
