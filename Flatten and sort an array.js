1fbba4336278f29c05396e71ff141f6491a0a803
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-24 08:37:04 +0500

    Update Flatten and sort an array.js
    shorter solution

--- Flatten and sort an array.js
+++ Flatten and sort an array.js
@@ -1,7 +1,5 @@
 "use strict";
 
 function flattenAndSort(array) {
-  return array
-    .reduce((acc, item) => acc.concat(item), [])
-    .sort((a, b) => a - b);
+  return [].concat(...array).sort((a, b) => a - b);
 }
