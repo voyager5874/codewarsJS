b9d838e07ab998b31138a2c0a58abe0fb29ba3cb
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-24 09:55:47 +0500

    Update Counting Duplicates.js
    we only need to know if a char appears  more than once, so 2 places is enough, though the time complexity  of the search via indexOf is the the same -> use object

--- Counting Duplicates.js
+++ Counting Duplicates.js
@@ -1,19 +1,8 @@
 function duplicateCount(text) {
-  const lowCaseText = text.toLowerCase();
-  const counts = [];
-  for (let i = 0; i < text.length; i++) {
-    let counter = 1;
-    for (let j = i + 1; j < text.length; j++) {
-      if (
-        lowCaseText[j] === lowCaseText[i] &&
-        lowCaseText.slice(0, i).indexOf(lowCaseText[i]) === -1
-      ) {
-        counter++;
-      }
-    }
-    if (counter > 1) {
-      counts.push(counter);
-    }
-  }
-  return counts.length;
+  return text
+    .toLowerCase()
+    .split("")
+    .filter(function (val, i, arr) {
+      return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+    }).length;
 }
