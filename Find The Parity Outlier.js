553904b482a43d2811b1e1b39f1e7801b2910434
Aleksandr Savkin <voyager5874@gmail.com>
2021-10-28 01:51:02 +0500

    Update Find The Parity Outlier.js
    one  loop instead of two (filter).  I could make it a little more verbose to avoid repetition of loooping for first 3 elements

--- Find The Parity Outlier.js
+++ Find The Parity Outlier.js
@@ -1,5 +1,27 @@
 function findOutlier(integers) {
-  const odd = integers.filter((item) => item % 2 !== 0);
-  const even = integers.filter((item) => item % 2 === 0);
-  return odd.length < even.length ? odd[0] : even[0];
+  let evenCount = 0;
+  let oddCount = 0;
+  const isOdd = (num) => num % 2 !== 0;
+  const isEven = (num) => num % 2 === 0;
+  for (let i = 0; i < 3; i++) {
+    if (isEven(integers[i])) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+  }
+  if (evenCount >= 2) {
+    let i = 0;
+    while (true) {
+      if (isOdd(integers[i])) return integers[i];
+      i++;
+    }
+  }
+  if (oddCount >= 2) {
+    let i = 0;
+    while (true) {
+      if (isEven(integers[i])) return integers[i];
+      i++;
+    }
+  }
 }
