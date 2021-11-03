9e1f648d782f7dcfb70faa6f7c4b807381d41095
Aleksandr Savkin <voyager5874@gmail.com>
2021-11-03 18:01:58 +0500

    Update Detect Pangram.js
    just use every function

--- Detect Pangram.js
+++ Detect Pangram.js
@@ -1,20 +1,6 @@
 function isPangram(string) {
-  if (string.length < 26) return false;
-  const found = {};
-  const ascii = Array(26)
-    .fill(0)
-    .map((element, index) => index + 97);
-  const alphabet = ascii.map((element) => String.fromCharCode(element));
-  for (const key of alphabet) {
-    found[key] = false;
-  }
-  for (let i = 0; i < string.length; i++) {
-    if (string[i].toLowerCase() !== string[i].toUpperCase()) {
-      found[string[i].toLowerCase()] = true;
-    }
-  }
-  for (let char in found) {
-    if (found[char] === false) return false;
-  }
-  return true;
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
+    return string.indexOf(x) !== -1;
+  });
 }
