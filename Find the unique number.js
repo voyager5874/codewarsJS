aaf753bdc6999d5fc08c3c40306d2c92d3a29eed
Aleksandr Savkin <voyager5874@gmail.com>
2021-11-08 14:37:54 +0300

    Update Find the unique number.js
    XOR it! 2,5 sec instead of 8

--- Find the unique number.js
+++ Find the unique number.js
@@ -1,8 +1,11 @@
 function findUnique(numbers) {
-  const counts = {};
-  for (const num of numbers) {
-    counts[num] = counts[num] ? counts[num] + 1 : 1;
-  }
-  const key = Object.keys(counts).find((key) => counts[key] === 1);
-  return parseInt(key);
+  return numbers.reduce((a, b) => a ^ b);
 }
+
+// ^ is XOR, and when you XOR two numbers, only bits that are different
+// between them become 1's in the result, so XOR'ing a number with itself will always result in 0.
+// The reduce method reduces an array to a single value by applying the supplied
+// function to each element in the array, for example if the function was adding two
+// numbers together the result would be the sum of the array. If you XOR all the numbers
+// in the array together, and all but one are duplicates, then each duplicate cancels itself
+// out in the process, and only the unique number remains.
