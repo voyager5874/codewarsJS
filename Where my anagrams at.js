f2fe3090ae00326f2a33a217a6ffc245a52d83a4
Aleksandr Savkin <voyager5874@gmail.com>
2021-11-19 12:00:43 +0300

    Update Where my anagrams at.js
    sort it!!!

--- Where my anagrams at.js
+++ Where my anagrams at.js
@@ -1,23 +1,4 @@
-function anagrams(word, words) {
-  return words.filter((item) =>
-    compareTwoObjects(analisis(word), analisis(item))
+let anagrams = (word, words) =>
+  words.filter(
+    (w) => w.split("").sort().join("") === word.split("").sort().join("")
   );
-}
-
-const analisis = (string) => {
-  const arr = string.split("");
-  const obj = {};
-  for (let item of arr) {
-    obj[item] ? obj[item]++ : (obj[item] = 1);
-  }
-  return obj;
-};
-
-const compareTwoObjects = (obj1, obj2) => {
-  for (let prop in obj1) {
-    if (obj1[prop] !== obj2[prop] || !obj2.hasOwnProperty(prop)) {
-      return false;
-    }
-  }
-  return true;
-};
